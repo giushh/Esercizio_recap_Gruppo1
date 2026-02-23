2726682332d63cac78c4d04c26273016b9a22d02
Fabio D'Alessandro <137040853+OvrTnkDev@users.noreply.github.com>
2026-02-23 11:42:23 +0100

    updt dataManager

--- Fabio/gestione_dati.py
+++ Fabio/gestione_dati.py
@@ -3,17 +3,19 @@
 
 import json as j
 
+"""Classe per la gestione dei dati, con metodi per leggere, scrivere e aggiungere dati ai file json,
+fatta statica perché non ha bisogno di essere istanziata, e con metodi di classe per accedere ai file json"""
 class DataManager:
     __FILE_UTENTI = r"db/utenti.json"
     __FILE_LOGIN = r"db/login.json"
     
     # Getter per la classe
     @classmethod
-    def get_file_utenti(cls):
+    def get_users_file(cls):
         return cls.__FILE_UTENTI
 
     @classmethod
-    def get_file_login(cls):
+    def get_login_file(cls):
         return cls.__FILE_LOGIN
 
     # Metodi per la gestione dei file json
@@ -42,3 +44,49 @@ class DataManager:
             cls.write_f(file, current_data)
         else:
             cls.write_f(file, [data])
+
+    #-> MI SERVE METODO esiste_utente(username) e salva_utente(studente).
+    @classmethod
+    def login_check(cls, username, password):
+        login_data = cls.read_f(cls.get_login_file())
+        if login_data is not None:
+            for login in login_data:
+                if login["username"] == username and login["password"] == password:
+                    return True
+        return False
+
+    @classmethod
+    def user_check(cls, username):
+        users = cls.read_f(cls.get_users_file())
+        if users is not None:
+            for user in users:
+                if user["username"] == username:
+                    return True
+        return False
+    
+    #-> MI SERVE METODO aggiorna_corso(username, nuovo corso)
+    @classmethod
+    def cours_update(cls, username, new_course):
+        users = cls.read_f(cls.get_users_file())
+        if users is not None:
+            for user in users:
+                if user["username"] == username:
+                    user["corso"] = new_course
+                    cls.write_f(cls.get_users_file(), users)
+                    return True
+        return False
+    
+    #-> MI SERVE METODO reset_studenti() e intervento(motivazione)
+    @classmethod
+    def reset_students(cls):
+        cls.write_f(cls.get_users_file(), [])
+        cls.write_f(cls.get_login_file(), [])
+    
+    #generatore id univoco per ogni studente, basato sul numero di studenti già presenti
+    @classmethod
+    def generate_id(cls):
+        users = cls.read_f(cls.get_users_file())
+        if users is not None:
+            return len(users) + 1
+        else:
+            return 1
